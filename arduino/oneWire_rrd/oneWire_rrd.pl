36d21e314d40eed9ef262a0c1d3262bd1f4565e9
gfbarros <gfbarros@fc7e7464-f6ed-11de-9bdd-55956ae4c37b>
2010-03-05 23:31:27 +0000

    updated graph to use RRDp

--- arduino/oneWire_rrd/oneWire_rrd.pl
+++ arduino/oneWire_rrd/oneWire_rrd.pl
@@ -27,15 +27,21 @@
 # MIN MAX?
 
 
-$base_path = "/opt/oneWire_rrd";         # main app path
-$rrdtool = "/usr/bin/rrdtool";           # rrdtool binary
-our $db = "$base_path/ow.rrd";           # directory where the database lives
-our $htdocs = "$base_path/www";          # directory where the graphs will end up
+$base_path = "/opt/oneWire_rrd";      # main app path
+our $rrdtool = "/usr/bin/rrdtool";    # rrdtool binary
+our $db = "$base_path/ow.rrd";        # directory where the database lives
+our $htdocs = "$base_path/www";       # directory where the graphs will end up
 our $remoteWeb = 'http://192.168.1.80';  # URL to get the data from
 
+our $height = 200;
+our $width = 800;
+
 use strict;
+use RRDp;
 use RRD::Simple;
 use LWP::Simple;
+use POSIX qw(strftime); # Used for strftime in graph() method
+
 
 our @TData; #where we store data (for now)
 my $rrd = RRD::Simple->new(
@@ -43,24 +49,30 @@ my $rrd = RRD::Simple->new(
     on_missing_ds => "die" );
 
 if ( defined $ARGV[0] ) {
-    if ( $ARGV[0] eq "graph" ) {
-        print "generating graphs...\n";
-        graphRRD();
-    }
+  if ( $ARGV[0] eq "graph" ) {
+    print "generating graphs...\n";
+    GraphRRD('end-1hour', 'now', "$htdocs/ow-1hourly.png", 'Hourly');
+    GraphRRD('end-6hour', 'now', "$htdocs/ow-6hourly.png", '6Hour');
+    GraphRRD('end-12hour', 'now', "$htdocs/ow-12hourly.png", '12Hour');
+    GraphRRD('end-1day', 'now', "$htdocs/ow-daily.png", 'Daily');
+    GraphRRD('end-1week', 'now', "$htdocs/ow-weekly.png", 'Weekly');
+    GraphRRD('end-1month', 'now', "$htdocs/ow-monthly.png", 'Monthly');
+    GraphRRD('end-1year', 'now', "$htdocs/ow-yearly.png", 'Yearly');
+  }
 
-    elsif ( $ARGV[0] eq "update" ) {
-        print "updating $db...";
-        getData();
-        print "got data...";
-        cleanup();
-        updateRRD();
-        print "updated!\n";
-    }
+  elsif ( $ARGV[0] eq "update" ) {
+      print "updating $db...";
+      getData();
+      print "got data...";
+      cleanup();
+      updateRRD();
+      print "updated!\n";
+  }
 
-    elsif ( $ARGV[0] eq "webdump" ) {
-        getData();
-        print "Got: @TData\n";
-    }
+  elsif ( $ARGV[0] eq "webdump" ) {
+    getData();
+    print "Got: @TData\n";
+  }
 
     elsif ($ARGV[0] eq "info") {
         getRRDinfo();
@@ -87,7 +99,7 @@ sub getData {
     my $content = get $remoteWeb;
     die "Couldn't get $remoteWeb" unless defined $content;
 
-    if( $content =~ m{CSV:([0-9,. ]+)} ) {
+    if( $content =~ m{CSV:([0-9,.\- ]+)} ) {
         @TData = split(/,/,$1);
     }
 }
@@ -114,29 +126,105 @@ sub updateRRD {
     );
 }
 
-sub graphRRD {
-    my %rtn = $rrd->graph(
-        destination => $htdocs,
-        basename => "ow",
-        timestamp => "both",
-        source_labels => { T1 => "Attic",
-                           T2 => "Basement",
-                           T3 => "Master Bedroom",
-                           T4 => "unused",
-                           T5 => "Utility Room",
-                           T6 => "Thermostat",
-                           T7 => "unused",
-                           T8 => "Kitchen",
-                           T9 => "Garage",
-                           T10 => "unused", },
-        line_thickness => 2,
-        extended_legend => 1,
-        title => "Temperatures",
-        vertical_label => "Degrees (F)",
-        width => 800, 
-        height => 200,
-        );
+
+sub GraphRRD {
+  my ( $starttime, $endtime, $ofname, $title ) = @_;
+
+  RRDp::start "$rrdtool";
+  RRDp::cmd "last $db";
+  my $lastupdate = RRDp::read;
+
+  my $timefmt = '%a %d/%b/%Y %T %Z';
+  my $rtime = sprintf('RRD last updated: %s\r', strftime($timefmt,localtime($$lastupdate)));
+  $rtime =~ s/:/\\:/g; 
+
+  my $gtime = sprintf('Graph last updated: %s\r', strftime($timefmt,localtime(time)));
+  $gtime =~ s/:/\\:/g;
+
+  RRDp::cmd
+    "graph $ofname --imgformat PNG",
+    "--start '$starttime' --end '$endtime'",
+    "--width $width --height $height",
+    "--slope-mode",
+    "--title $title",
+    "--vertical-label 'Degrees (F)'",
+    "DEF:T1=$db:T1:AVERAGE",
+    "DEF:T2=$db:T2:AVERAGE",
+    "DEF:T3=$db:T3:AVERAGE",
+    "DEF:T4=$db:T4:AVERAGE",
+    "DEF:T5=$db:T5:AVERAGE",
+    "DEF:T6=$db:T6:AVERAGE",
+    "DEF:T7=$db:T7:AVERAGE",
+    "DEF:T8=$db:T8:AVERAGE",
+    "DEF:T9=$db:T9:AVERAGE",
+    "DEF:T10=$db:T10:AVERAGE",
+#line1
+    "COMMENT:\"       \"",
+    "COMMENT:\"             Min       Max      Avg      Last\"",
+    "COMMENT:\"            \"",
+    "COMMENT:\"             Min       Max      Avg      Last\\n\"",
+#line2
+    "COMMENT:\"     \"",
+    "LINE2:T1#FF8000:'Attic     '",
+    "GPRINT:T1:MIN:\"%5.2lf F\"",
+    "GPRINT:T1:MAX:\"%5.2lf F\"",
+    "GPRINT:T1:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T1:LAST:\"%5.2lf F\"",
+    "COMMENT:\"      \"",
+    "LINE2:T5#008000:'Utility Room'",
+    "GPRINT:T5:MIN:\"%5.2lf F\"",
+    "GPRINT:T5:MAX:\"%5.2lf F\"",
+    "GPRINT:T5:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T5:LAST:\"%5.2lf F\\n\"",
+#line3
+    "COMMENT:\"     \"",
+    "LINE2:T3#804099:'Master Bed'",
+    "GPRINT:T3:MIN:\"%5.2lf F\"",
+    "GPRINT:T3:MAX:\"%5.2lf F\"",
+    "GPRINT:T3:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T3:LAST:\"%5.2lf F\"",
+    "COMMENT:\"      \"",
+    "LINE2:T2#c0b000:'Basement    '",
+    "GPRINT:T2:MIN:\"%5.2lf F\"",
+    "GPRINT:T2:MAX:\"%5.2lf F\"",
+    "GPRINT:T2:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T2:LAST:\"%5.2lf F\\n\"",
+#line4
+    "COMMENT:\"     \"",
+    "LINE2:T6#000090:'Thermostat'",
+    "GPRINT:T6:MIN:\"%5.2lf F\"",
+    "GPRINT:T6:MAX:\"%5.2lf F\"",
+    "GPRINT:T6:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T6:LAST:\"%5.2lf F\"",
+    "COMMENT:\"      \"",
+    "LINE2:T9#000000:'Garage      '",
+    "GPRINT:T9:MIN:\"%5.2lf F\"",
+    "GPRINT:T9:MAX:\"%5.2lf F\"",
+    "GPRINT:T9:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T9:LAST:\"%5.2lf F\\n\"",
+#line5
+    "COMMENT:\"     \"",
+    "LINE2:T8#a02020:'Kitchen   '",
+    "GPRINT:T8:MIN:\"%5.2lf F\"",
+    "GPRINT:T8:MAX:\"%5.2lf F\"",
+    "GPRINT:T8:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T8:LAST:\"%5.2lf F\"",
+    "COMMENT:\"      \"",
+    "LINE2:T10#4575d7:'Outside     '",
+    "GPRINT:T10:MIN:\"%5.2lf F\"",
+    "GPRINT:T10:MAX:\"%5.2lf F\"",
+    "GPRINT:T10:AVERAGE:\"%5.2lf F\"",
+    "GPRINT:T10:LAST:\"%5.2lf F\\n\"",
+    "COMMENT:\"\\s\"",
+#line6
+    "COMMENT:\"$gtime\"",
+    "COMMENT:\"$rtime\""
+;
+
+  my $answer=RRDp::read;
+  RRDp::end;
 }
+
 
 sub getRRDinfo() {
     my $lastUpdated = $rrd->last;
