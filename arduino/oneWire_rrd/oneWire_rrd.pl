bb4a3810af95f315da71e8fbe37c6c7fa7095bd0
gfbarros@gmail.com <gfbarros@gmail.com@fc7e7464-f6ed-11de-9bdd-55956ae4c37b>
2011-01-14 01:16:24 +0000

    major updates to support chumby graphs, moved 1s deta out of rrd for performance and created new rrd to support long-term graphing

--- arduino/oneWire_rrd/oneWire_rrd.pl
+++ arduino/oneWire_rrd/oneWire_rrd.pl
@@ -1,6 +1,6 @@
-#!/usr/bin/perl
+#!/psp/usr/bin/perl
 
-## rrd created with:
+## original rrd created with:
 #rrdtool create ow.rrd \
 #--step '60' \
 #'DS:T1:GAUGE:240:-50:150' \
@@ -23,15 +23,48 @@
 #
 # 1min interval saved for 2 years
 #
-# AVERAGE for temp, LAST for hvac
-# MIN MAX?
+## new rrd created with:
+#rrdtool create filename.rrd \
+#--step '60' \
+#--start '1230768000' \
+#'DS:T1:GAUGE:600:-50:150' \
+#'DS:T2:GAUGE:600:-50:150' \
+#'DS:T3:GAUGE:600:-50:150' \
+#'DS:T4:GAUGE:600:-50:150' \
+#'DS:T5:GAUGE:600:-50:150' \
+#'DS:T6:GAUGE:600:-50:150' \
+#'DS:T7:GAUGE:600:-50:150' \
+#'DS:T8:GAUGE:600:-50:150' \
+#'DS:T9:GAUGE:600:-50:150' \
+#'DS:T10:GAUGE:600:-50:150' \
+#'DS:HVAC:GAUGE:120:-10:10' \
+#'RRA:AVERAGE:0.5:1:20160' \
+#'RRA:AVERAGE:0.5:30:1488' \
+#'RRA:MIN:0.5:30:1488' \
+#'RRA:MAX:0.5:30:1488' \
+#'RRA:AVERAGE:0.5:60:26280' \
+#'RRA:MIN:0.5:60:26280' \
+#'RRA:MAX:0.5:60:26280'
+
+#  T1 =  Attic
+#  T2 =  Basement
+#  T3 =  Master Bed
+#  T4 =  //dead
+#  T5 =  Utility room
+#  T6 =  thermostat
+#  T7 =  arduino local // dead
+#  T8 =  kitchen
+#  T9 =  garage
+#  T10 = outside
+#  HVAC = hvac status
 
 
-$base_path = "/opt/oneWire_rrd";      # main app path
-our $rrdtool = "/usr/bin/rrdtool";    # rrdtool binary
-our $db = "$base_path/ow.rrd";        # directory where the database lives
+$base_path = "/mnt/usb/oneWire_rrd";      # main app path
+our $rrdtool = "/mnt/usb/rrdtool/bin/rrdtool";    # rrdtool binary
+our $db = "$base_path/ow.rrd";        # path to rrd file                          
+our $rawdb = "$base_path/ow.raw";     # path to raw file
 our $htdocs = "$base_path/www";       # directory where the graphs will end up
-our $remoteWeb = 'http://192.168.1.80';  # URL to get the data from
+our $remoteWeb = 'http://127.0.0.1/cgi-bin/getTemps.pl';  # URL to get the data from
 
 our $height = 200;
 our $width = 800;
@@ -44,35 +77,40 @@ use POSIX qw(strftime); # Used for strftime in graph() method
 
 
 our @TData; #where we store data (for now)
+our %temps; #key=name_of_temp, value=temp_or_hum
+our %hvac; #store hvac info like status, times, etc
+
 my $rrd = RRD::Simple->new(
     file => $db,
     on_missing_ds => "die" );
 
 if ( defined $ARGV[0] ) {
   if ( $ARGV[0] eq "graph" ) {
-    print "generating graphs...\n";
-    GraphRRD('end-1hour', 'now', "$htdocs/ow-1hourly.png", 'Temperature - Last Hour');
-    GraphRRD('end-6hour', 'now', "$htdocs/ow-6hourly.png", 'Temperature - Last 6 Hours');
-    GraphRRD('end-12hour', 'now', "$htdocs/ow-12hourly.png", 'Temperature - Last 12 Hours');
-    GraphRRD('end-1day', 'now', "$htdocs/ow-daily.png", 'Temperature - Last 24 Hours');
-    GraphRRD('end-1week', 'now', "$htdocs/ow-weekly.png", 'Temperature - Last 7 Days');
-    GraphRRD('end-1month', 'now', "$htdocs/ow-monthly.png", 'Temperature - Last 30 Days');
-    GraphRRD('end-1year', 'now', "$htdocs/ow-yearly.png", 'Temperature - Past Year');
+      GraphChumby();
+      GraphWeb();
   }
 
+  elsif ( $ARGV[0] eq "chumby" ) {
+      GraphChumby();
+  }
+  
+  elsif ( $ARGV[0] eq "webgraph" ) {
+      GraphWeb();
+  }
+  
   elsif ( $ARGV[0] eq "update" ) {
       my $nowtime = localtime();
-      print "nowtime -> updating $db...";
+      print "$nowtime -> updating $db...";
       getData();
       print "got data...";
-      cleanup();
       updateRRD();
+      updateRAW();
       print "updated!\n";
   }
 
   elsif ( $ARGV[0] eq "webdump" ) {
-    getData();
-    print "Got: @TData\n";
+      getData();
+      print "Got: @TData\n";
   }
 
   elsif ($ARGV[0] eq "info") {
@@ -84,7 +122,7 @@ if ( defined $ARGV[0] ) {
   }
 
   else {
-    usage(); 
+      usage(); 
   }
 }
 else {
@@ -92,11 +130,14 @@ else {
 }
 
 sub usage {
-    print "Usage: onewire-graph graph|update|webdump|info\n";
-    print "   graph   - Builds graphs\n";
-    print "   update  - Reads data, inserts into rrd file\n";
-    print "   webdump - Reads data, dumps to console\n";
-    print "   info    - Dumps rrd info to console\n";
+    print "Usage: oneWire_rrd.pl graph|update|webdump|info\n";
+    print "   graph    - Builds all graphs\n";
+    print "   chumby   - Builds chumby graphs only\n";
+    print "   webgraph - Builds web graphs only\n";
+    print "   update   - Reads data, inserts into rrd file\n";
+    print "   webdump  - Reads data, dumps to console\n";
+    print "   info     - Dumps rrd info to console\n";
+    print "   hvac     - HVAC runtime over last 24hrs\n";
 }
 
 
@@ -104,35 +145,109 @@ sub getData {
     my $content = get $remoteWeb;
     die "Couldn't get $remoteWeb" unless defined $content;
 
-    if( $content =~ m{CSV:([0-9,.\- ]+)} ) {
-        @TData = split(/,/,$1);
+    if( $content =~ m{startvar&([\S]+)} ) {
+        @TData = split(/&/, $1);
+        
+        foreach my $pair (@TData) {
+            (my $key, my $value) = split(/=/, $pair);
+            
+            #pull out non-temp values! (HVACstatus value is text)
+            if ($key ne 'HVACstatus') { $temps{$key} = $value; }
+            else { $hvac{status} = $value; }
+        }
+        #trim to to 2 decimal places - worth looking into rounding later
+        #FUTUREFEATURE                                       
+        foreach my $key (keys %temps) {                  
+            $temps{$key} = sprintf("%.2f", $temps{$key});               
+        } 
     }
 }
 
-sub cleanup() {
-    for(my $i = 0; $i < @TData; $i++) {
-        #cleanup
-        $TData[$i] = sprintf("%.1f", $TData[$i]);
-    }
-}
 
 sub updateRRD {
     $rrd->update(
-        T1=>$TData[0],
-        T2=>$TData[1],
-        T3=>$TData[2],
-        T4=>$TData[3],
-        T5=>$TData[4],
-        T6=>$TData[5],
-        T7=>$TData[6],
-        T8=>$TData[7],
-        T9=>$TData[8],
-        T10=>$TData[9],
-#        HVAC=>'NaN'
-        HVAC=>$TData[10]
+        T1=>$temps{Attic},
+        T2=>$temps{Basement},
+        T3=>$temps{MasterBed},
+        T4=>'NaN',
+        T5=>$temps{Utility},
+        T6=>$temps{Thermostat},
+#        T6=>'NaN',
+#        T7=>$TData[6],
+        T7=>'NaN',
+        T8=>$temps{Kitchen},
+        T9=>$temps{Garage},
+        T10=>$temps{Outside},
+#        HVAC=>$hvac{status}
     );
 }
 
+sub updateRAW {                                                     
+    my @tmpData = @TData;                                       
+    unshift(@tmpData, time());                  
+    my $output = join(',', @tmpData);
+#   print $output;                  
+    my $ret = `echo $output >> $rawdb`;
+}
+
+sub GraphChumby {
+    my %rtn = $rrd->graph(
+        destination => $htdocs,
+        basename => "cby",
+        timestamp => "both",
+        periods => [qw(hour 6hour 12hour day week month annual)],
+        sources => [qw(T1 T2 T3 T5 T6 T8 T9 T10)],
+	source_labels => {
+	    T1 => "Attic",
+	    T2 => "Basement",
+	    T3 => "Master Bed",
+#	    T4 => "",
+	    T5 => "Utility Room",
+	    T6 => "Thermostat",
+#	    T7 => "",
+	    T8 => "Kitchen",
+	    T9 => "Garage",
+	    T10 => "Outside",
+#	    HVAC => "HVAC Status"
+	},
+	source_colors => {
+	    T1 => "ff8000",
+	    T2 => "c0b000",
+	    T3 => "804099",
+#	    T4 => "000000",
+	    T5 => "008000",
+	    T6 => "000090",
+#	    T7 => "000000",
+	    T8 => "a02020",
+	    T9 => "000000",
+	    T10 => "4575d7",
+#	    HVAC => "000000"
+	},
+	line_thickness => 2,
+	width => "673",
+	height => "145",
+#	full-size-mode => 1,
+    );
+}
+
+sub GraphWeb {
+    print "generating graphs...";
+    print "1h...";
+    GraphRRD('end-1hour', 'now', "$htdocs/ow-1hourly.png", 'Temperature - Last Hour');
+    print "6h...";
+    GraphRRD('end-6hour', 'now', "$htdocs/ow-6hourly.png", 'Temperature - Last 6 Hours');
+    print "12h...";
+    GraphRRD('end-12hour', 'now', "$htdocs/ow-12hourly.png", 'Temperature - Last 12 Hours');
+    print "day...";
+    GraphRRD('end-1day', 'now', "$htdocs/ow-daily.png", 'Temperature - Last 24 Hours');
+    print "week...";
+    GraphRRD('end-1week', 'now', "$htdocs/ow-weekly.png", 'Temperature - Last 7 Days');
+    print "month...";
+    GraphRRD('end-1month', 'now', "$htdocs/ow-monthly.png", 'Temperature - Last 30 Days');
+    print "year...";
+    GraphRRD('end-1year', 'now', "$htdocs/ow-yearly.png", 'Temperature - Past Year');
+    print "done generating graphs\n";
+}
 
 sub GraphRRD {
   my ( $starttime, $endtime, $ofname, $title ) = @_;
